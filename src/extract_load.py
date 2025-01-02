323b0d1673169731d2e3c02d2f432492ca7ed999
Bruno Silva <bruno.ogs@gmail.com>
2025-01-02 16:40:22 -0500

    update

--- src/extract_load.py
+++ src/extract_load.py
@@ -6,8 +6,22 @@ from dotenv import load_dotenv
 import os
 
 # import var de ambiente
+load_dotenv()
 
 commodities = ['CL=F', 'GC=F', 'SI=F']  # Petróleo bruto, Ouro, Prata
+
+# Obter as variáveis do arquivo .env
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+# Criar a URL de conexão do banco de dados
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
 
 def buscar_dados_commodities(simbolo, periodo='5d', intervalo="1d"):
     ticker = yf.Ticker('CL=F')
@@ -30,7 +44,9 @@ def buscar_todos_dados_commodities(commodities):
 
 # salvar no banco de dados
 
+def salvar_no_postgres(df, schema='public'):
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
+
 if __name__ == "__main__":
     dados_concatenados = buscar_todos_dados_commodities(commodities)
-    print(dados_concatenados)
-    #salvar_no_postgres(dados_concatenados, schema='public')
+    salvar_no_postgres(dados_concatenados, schema='public')
